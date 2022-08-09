99e144e366d6c40fe3e79292e01099a650062581
RostyslavPopadynets <rostyslav.popadynets@gmail.com>
2022-08-09 15:24:38 +0300

    Added google analitics for Gruntysko

--- public/js/app.js
+++ public/js/app.js
@@ -3,11 +3,6 @@ let rootUrl = "https://murmuring-earth-34683.herokuapp.com/";
 let elements = 0;
 
 $(document).ready(function () {
-    window.dataLayer = window.dataLayer || [];
-    function gtag(){dataLayer.push(arguments);}
-    gtag('js', new Date());
-
-    gtag('config', 'UA-233513543-1');
     getLaptops();
 });
 
